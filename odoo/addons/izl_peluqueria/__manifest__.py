49c8dbcca515a9c085c4f14f3925beada58953ed
Ian Zapatero López <ianzapa52@gmail.com>
2026-01-18 12:51:40 +0100

    Estructura inicial del módulo izl_peluqueria

---
+++ odoo/addons/izl_peluqueria/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Peluquería IZL',
+    'version': '1.0',
+    'author': 'Ian Zapatero López',
+    'category': 'Servicios',
+    'summary': 'Gestión de clientes, citas, empleados y servicios de la peluquería',
+    'depends': ['base'],
+    'installable': True,
+    'application': True
+}
