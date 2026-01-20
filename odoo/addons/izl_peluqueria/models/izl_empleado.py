f7a6967e4939527f543d94b7d22f23c600c097f0
Ian Zapatero López <ianzapa52@gmail.com>
2026-01-20 17:19:36 +0100

    Corrección: añadida vista anidada para campo many2many en formulario de servicios

--- odoo/addons/izl_peluqueria/models/izl_empleado.py
+++ odoo/addons/izl_peluqueria/models/izl_empleado.py
@@ -1,11 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class IzlEmpleado(models.Model):
     _name = 'izl_peluqueria.empleado'
     _description = 'Empleados de la peluquería'
+
+    name = fields.Char(string='Nombre completo', required=True)
 
     nombre = fields.Char(string='Nombre', required=True)
     apellido = fields.Char(string='Apellido', required=True)
     telefono = fields.Char(string='Teléfono')
     email = fields.Char(string='Correo electrónico')
     especialidad = fields.Char(string='Especialidad')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('name') and vals.get('nombre') and vals.get('apellido'):
+                vals['name'] = f"{vals['nombre']} {vals['apellido']}"
+        return super().create(vals_list)
